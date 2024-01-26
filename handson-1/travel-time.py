d11e835d8976247ebdf3ba2599259e9a62ea3d0f
Jada Snyder <jxs1222@sru.edu>
2024-01-26 11:35:58 -0500

    changes for travel-time.py

--- handson-1/travel-time.py
+++ handson-1/travel-time.py
@@ -1,11 +1,11 @@
 miles = int(input("Enter Miles: "))
 mph = int(input("Enter Miles per Hour: "))
 
-
+#calculating total minutes and hours
 total_minutes = miles * 60 // mph
 hours = total_minutes //mph
 minutes = total_minutes % mph
-
+#printing results
 print("Travel Time Calculator")
 print("Estimated Travel Time")
 print(f"Hours: {hours}")
